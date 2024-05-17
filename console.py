de0476027221c4edaa98af9580fa86eb2ad93b61
Ajobiewe Joseph <joseph4hacking@gmail.com>
2024-05-17 00:51:32 -0700

    moving to my desktop to continue work

--- console.py
+++ console.py
@@ -11,4 +11,32 @@ class HBNBCommand(cmd.Cmd):
     """
     A command intepreter for the aurbnb console
     """
-    pass
+    quote = "(hbnb)"
+
+    def do_quit(self, line):
+        """
+        Quit command to exit the program
+        """
+        return (True)
+
+    def help_quit(self):
+        """
+        Quit command to exit the program
+        """
+        print("Quit command to exit the program")
+
+    def do_EOF(self, line):
+        """
+        Ctrl_D triggers a program exit
+        """
+        return (True)
+
+    def emptyline(self):
+        """
+        Do nothing
+        """
+        pass
+
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
---
+++ tests/test_console.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Test module for the console app
+"""
+import unittest
+
+
+class TestConsole(unittest.TestCase):
+    """
+    Series of test cases for the functionalities our
+    console provides
+    """
+    pass

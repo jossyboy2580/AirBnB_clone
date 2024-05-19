9c6de827039f2f83337909fb6260538d33939af3
Ajobiewe Joseph <joseph4hacking@gmail.com>
2024-05-20 00:19:58 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -16,7 +16,7 @@ class BaseModel:
         """
         Initialize an instance of our class
         """
-        if kwargs > 0:
+        if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     value = datetime.fromisoformat(value)
